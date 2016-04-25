017c598dd07effc32e138b8cdb02e17f55028870
janwi <janwi@JWILD-NB>
2016-04-25 16:20:39 +0200

    Add project files.

---
+++ Solarizr/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Solarizr.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Solarizr.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
